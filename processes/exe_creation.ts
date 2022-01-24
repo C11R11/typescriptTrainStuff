02a0fe523c5ee1aed2d4f54ad68a1349528055ed
Cristian Rodriguez Canto <cristian.rodriguez@maptek.cl>
2022-01-24 11:50:46 -0300

    exe creation multiplatform

---
+++ processes/exe_creation.ts
@@ -0,0 +1,6 @@
+// To build this is needed to use --> pgk exe_creation.js (javascript)
+
+setTimeout(() => process.stdout.write("Hello 8000\n"), 8000);
+setTimeout(() => process.stdout.write("Hello 2000\n"), 2000);
+setTimeout(() => process.stdout.write("Hello 4000\n"), 4000);
+setTimeout(() => process.stdout.write("Hello 5000\n"), 5000);
--- processes/exe_node.ts
+++
@@ -1,6 +0,0 @@
-
-
-setTimeout(() => process.stdout.write("Hello 8000\n"), 8000);
-setTimeout(() => process.stdout.write("Hello 2000\n"), 2000);
-setTimeout(() => process.stdout.write("Hello 4000\n"), 4000);
-setTimeout(() => process.stdout.write("Hello 5000\n"), 5000);
